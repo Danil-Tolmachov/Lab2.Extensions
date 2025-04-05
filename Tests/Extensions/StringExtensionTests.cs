988339aabf01f512c0bc649ddd28eb96679b91c6
astymic <max1clash2coc@gmail.com>
2025-04-05 20:35:45 +0200

    Added StringExtension Unit-Test

--- Tests/Extensions/StringExtensionTests.cs
+++ Tests/Extensions/StringExtensionTests.cs
@@ -1,5 +1,28 @@
-﻿namespace Tests.Extensions;
+﻿using Lab2.Extensions;
 
-internal class StringExtensionTests
+namespace Tests.Extensions;
+
+public class StringExtensionTests
 {
+    [Theory]
+    [InlineData("too many word here", "ereh drow ynam oot")]
+    public void Reverse_Works(string input, string expected)
+    {
+        // Act 
+        var actual = input.CustomReverse();
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("too many word here", "o", 3)]
+    public void Count_Works(string input, string match, int expected)
+    {
+        // Act 
+        var actual = input.CustomCount(match);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 }
